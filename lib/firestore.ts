f3c9d900135d46ec6f6f28002f6ea5685634816d
moathssnck <myneww00@gmail.com>
2025-08-03 02:39:22 -0700

    ssss

--- lib/firestore.ts
+++ lib/firestore.ts
@@ -3,12 +3,13 @@ import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   // Your Firebase config will go here
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBYrLI1nXUX-fxem9pLVUYog4-ApAgdbl8",
+  authDomain: "jonvbapp.firebaseapp.com",
+  projectId: "jonvbapp",
+  storageBucket: "jonvbapp.firebasestorage.app",
+  messagingSenderId: "853579155809",
+  appId: "1:853579155809:web:11b8177eb64c864dfd5d20",
+  measurementId: "G-J44N9Q40E6"
 }
 
 const app = initializeApp(firebaseConfig)
